04fca9e02682cb1c0976d28478f6cb25bed98bca
Priyak1107 <priyak1107@gmail.com>
2017-01-06 22:19:11 +0530

    Added Test Order of Execution

--- src/niki_ai/nikiApp.java
+++ src/niki_ai/nikiApp.java
@@ -1,13 +1,13 @@
 package niki_ai;
 
-import io.appium.java_client.android.AndroidDriver;
-
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.CapabilityType;
@@ -16,7 +16,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class nikiApp {
 
 	WebDriver driver;
@@ -32,12 +32,10 @@ public class nikiApp {
 		capabilities.setCapability("appPackage", "com.nikiapp");
 		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		Thread.sleep(5000);
-
-
 	}
 
 	@Test
-	public void blankUserNameTest()
+	public void test1_blankUserNameTest()
 	{
 		driver.findElement(By.id("com.nikiapp:id/login_username")).sendKeys("");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -53,7 +51,7 @@ public class nikiApp {
 
 
 	@Test
-	public void addUserName()
+	public void test2_addUserName()
 	{
 		driver.findElement(By.id("com.nikiapp:id/login_username")).sendKeys("hi@xyz.com");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -68,7 +66,7 @@ public class nikiApp {
 	}
 
 	@Test
-	public void sendGroupChatMessage(){
+	public void test3_sendGroupChatMessage(){
 		driver.findElement(By.name("GroupChat")).click();
 		driver.findElement(By.name("Enter message")).sendKeys("hi");
 		driver.findElement(By.id("com.nikiapp:id/sendMessage")).click();
@@ -77,14 +75,14 @@ public class nikiApp {
 	}
 
 	@Test
-	public void verifySentGroupMessage(){
+	public void test4_verifySentGroupMessage(){
 		driver.findElement(By.name("GroupChat")).click();
 		String message = driver.findElement(By.id("com.nikiapp:id/chatMessage")).getText();
 		Assert.assertEquals("hi", message);
 	}
 
 	@Test
-	public void closeGroupChat(){
+	public void test5_closeGroupChat(){
 		driver.findElement(By.className("android.widget.ImageButton")).click();	
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		try{
@@ -96,7 +94,7 @@ public class nikiApp {
 	}
 
 	@Test
-	public void createPrivateChatWithUser() {
+	public void test6_createPrivateChatWithUser() {
 		driver.findElement(By.className("android.widget.ImageButton")).click();
 		driver.findElement(By.id("com.nikiapp:id/privateChat")).click();
 		driver.findElement(By.id("com.nikiapp:id/editTextDialogUserInput")).sendKeys("sitaram");
@@ -104,13 +102,13 @@ public class nikiApp {
 	}
 
 	@Test
-	public void openPrivateChat(){
+	public void test7_openPrivateChat(){
 		driver.findElement(By.name("Sitram")).click();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 	@Test
-	public void closePrivateChat(){
+	public void test8_closePrivateChat(){
 		driver.findElement(By.className("android.widget.ImageButton")).click();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
